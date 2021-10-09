767e8b796356cc0ec200752b915ff1a71fd40a14
RitaDyvnych <m.dyvnych@gmail.com>
2021-10-09 16:45:37 +0300

    написаны шаг 1 и шаг 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClickGood = () => {
+    this.setState((prevState) => {
+      return { good: prevState.good + 1 };
+    });
+  };
+  handleClickNeutral = () => {
+    this.setState((prevState) => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+  handleClickBad = () => {
+    this.setState((prevState) => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const countTotalFeedback = good + neutral + bad;
+    const countPositiveFeedbackPercentage = Math.round(
+      (good / countTotalFeedback) * 100
+    );
+    return (
+      <div className="App">
+        <h2>Please leave feedback</h2>
+        <button onClick={this.handleClickGood}>Good</button>
+        <button onClick={this.handleClickNeutral}>Neutral</button>
+        <button onClick={this.handleClickBad}>Bad</button>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {countTotalFeedback}</p>
+        <p>Positive feedback: {countPositiveFeedbackPercentage}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
