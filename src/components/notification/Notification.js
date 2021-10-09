53fc65fe98dc2c45b94d8c2b08444d9fb3f9b828
RitaDyvnych <m.dyvnych@gmail.com>
2021-10-09 17:51:08 +0300

    добавлены стили для нотификаций

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import style from "../statistics/statistics.module.css";
 
 export default function Notification({ msg }) {
-  return <div>{msg}</div>;
+  return <div className={style.title}>{msg}</div>;
 }
 
 Notification.propTypes = {
